3a2c02e2ea2d3ce0fb966563a90aac889581f572
julio yescas <julioyescas7@gmail.com>
2023-04-27 19:32:45 -0600

    algunos problemas de accesibilidad y de estructura

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import {projects} from './info';
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { projects } from "./info";
 
 function App() {
-
   return (
-        <>
-
-        <header>
-        <div className="fondo">
-          
-        </div>
+    <>
+      <header>
+        <div className="fondo"></div>
         <Navbar className="nav-color">
-            <Container >
-              <Navbar.Brand href="#home" className="font-style">Navbar</Navbar.Brand>
-              <Nav className="#">
-                <Nav.Link href="#home" className="font-style">Home</Nav.Link>
-                <Nav.Link href="#features" className="font-style">Features</Nav.Link>
-                <Nav.Link href="#pricing" className="font-style">Pricing</Nav.Link>
-              </Nav>
-            </Container>
-          </Navbar>
-        </header>
-          
-        <div className="presentation">
-          <div className="mini-card">
-           <h1>Julio Yescas</h1>
-           <h3>ing sistemas specialist on web development</h3>
-          </div>
-        </div>
-        <div className="center">
-          <div>
-          <h2>about me</h2>
-          <p>unofficial ing. in computational system, specialist on web development <br></br>with c1 on english and n5 in japanese</p>
-          </div>
-          <div>
-            <img src="https://i.pinimg.com/564x/55/32/a3/5532a35c10e7558b3eed821cd8978aa4.jpg" alt="zorrito"/>
-          </div>
-        </div>
-        
+          <Container>
+            <Navbar.Brand href="#home" className="font-style">
+              Navbar
+            </Navbar.Brand>
+            <Nav className="#">
+              <Nav.Link href="#home" className="font-style">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#features" className="font-style">
+                Features
+              </Nav.Link>
+              <Nav.Link href="#pricing" className="font-style">
+                Pricing
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
 
-    <div className="mini-card">
-      <h2>projects</h2>
-      <h4>just a little of my work</h4>
-    </div>
+      <div className="presentation">
+        <div className="mini-card">
+          <h1>Julio Yescas</h1>
+          <h2>ing sistemas specialist on web development</h2>
+        </div>
+      </div>
+      <div className="center">
+        <div>
+          <h3>about me</h3>
+          <p>
+            unofficial ing. in computational system, specialist on web
+            development <br></br>with c1 on english and n5 in japanese
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://i.pinimg.com/564x/55/32/a3/5532a35c10e7558b3eed821cd8978aa4.jpg"
+            alt="zorrito"
+          />
+        </div>
+      </div>
 
-    
-    <div className="center">
-    {projects.map((nombrecito) => 
-         
-          <Card style={{ width: '18rem' }}>
-             <Card.Img variant="top" src={nombrecito.image} />
-             <Card.Body>
+      <div className="mini-card">
+        <h2>projects</h2>
+        <h3>just a little of my work</h3>
+      </div>
+
+      <div className="center">
+        {projects.map((nombrecito) => (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src={nombrecito.image}
+              alt={nombrecito.alternativo}
+            />
+            <Card.Body>
               <Card.Title>{nombrecito.name}</Card.Title>
-                <Card.Text>
-                 {nombrecito.description}
-               </Card.Text>
-                <a href={nombrecito.link} className="Button">lets go</a>
-             </Card.Body>
-            </Card>
-        )}
-    </div>
-
-     
-        </>
-      
-    
-    
-   
-  )
+              <Card.Text>{nombrecito.description}</Card.Text>
+              <a href={nombrecito.link} className="Button">
+                lets go
+              </a>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/info.ts
+++ src/info.ts
@@ -1,40 +1,47 @@
 export const projects = [
-    {
-        Key: "1",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/1200px-Netflix_logo.svg.png",
-        name: "netfliz",
-        description: "esta pagina es para ver videitos",
-        link: "https://www.netflix.com/mx/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse"
-    },
-    {
-        Key: "2",
-        image:"https://i.pinimg.com/564x/b3/71/dd/b371dde5f4a8a4dab220f9f7684bde44.jpg",
-        name: "netfliz",
-        description: "esta pagina es para ver videitos",
-        link: "https://www.netflix.com/mx/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse"
-    },
-    {
-        Key: "3",
-        image:"https://i.pinimg.com/564x/26/b8/55/26b8553f4d688919b03b532e05d7e9d7.jpg",
-        name: "netfliz",
-        description: "esta pagina es para ver videitos",
-        link: "https://www.netflix.com/mx/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse"
-    },
-    {
-        Key: "4",
-        image:"https://i.pinimg.com/564x/e0/4c/5d/e04c5daaf5ee55fd230c792fe830152c.jpg",
-        name: "netfliz",
-        description: "esta pagina es para ver videitos",
-        link: "https://www.netflix.com/mx/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse"
-    },
-    {
-        Key: "5",
-        image:"https://i.pinimg.com/564x/82/14/07/8214071000196cebe078ceae5eb2f592.jpg",
-        name: "netfliz",
-        description: "esta pagina es para ver videitos",
-        link: "https://www.netflix.com/mx/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse"
-    }
-
-
-
-]
+  {
+    Key: "1",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/1200px-Netflix_logo.svg.png",
+    name: "netfliz",
+    description: "esta pagina es para ver videitos",
+    link: "https://www.netflix.com/mx/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse",
+    alternativo: "dibujo de una imagen",
+  },
+  {
+    Key: "2",
+    image:
+      "https://i.pinimg.com/564x/b3/71/dd/b371dde5f4a8a4dab220f9f7684bde44.jpg",
+    name: "netfliz",
+    description: "esta pagina es para ver videitos",
+    link: "https://www.youtube.com/",
+    alternativo: "dibujo de una imagen",
+  },
+  {
+    Key: "3",
+    image:
+      "https://i.pinimg.com/564x/26/b8/55/26b8553f4d688919b03b532e05d7e9d7.jpg",
+    name: "netfliz",
+    description: "esta pagina es para ver videitos",
+    link: "https://www.pinterest.com.mx/",
+    alternativo: "dibujo de una imagen",
+  },
+  {
+    Key: "4",
+    image:
+      "https://i.pinimg.com/564x/e0/4c/5d/e04c5daaf5ee55fd230c792fe830152c.jpg",
+    name: "netfliz",
+    description: "esta pagina es para ver videitos",
+    link: "https://www.amazon.com/",
+    alternativo: "dibujo de una imagen",
+  },
+  {
+    Key: "5",
+    image:
+      "https://i.pinimg.com/564x/82/14/07/8214071000196cebe078ceae5eb2f592.jpg",
+    name: "netfliz",
+    description: "esta pagina es para ver videitos",
+    link: "https://www.mercadolibre.com.mx/",
+    alternativo: "dibujo de una imagen",
+  },
+];
